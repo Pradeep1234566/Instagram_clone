e215468c40521f051033b689938aff2b3e85cd16
Pradeep <pawarpradeep1707@gmail.com>
2025-01-14 13:22:54 +0530

    Likes also done

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -4,7 +4,11 @@ class MyPost extends StatelessWidget {
   final String caption;
   final String path;
   final String name;
-  const MyPost({super.key, required this.caption, required this.path, required this.name});
+  const MyPost(
+      {super.key,
+      required this.caption,
+      required this.path,
+      required this.name});
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +56,7 @@ class MyPost extends StatelessWidget {
           children: [
             Row(
               children: [
-                Icon(Icons.favorite_border),
+                Icon(Icons.favorite),
                 SizedBox(width: 10),
                 Icon(
                   Icons.comment,
@@ -69,7 +73,37 @@ class MyPost extends StatelessWidget {
             Icon(Icons.bookmark_outline)
           ],
         ),
-        SizedBox(height: 10),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0, left: 8.0, top: 3),
+              child: Text('Liked by'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 3.0, top: 3, right: 2.0),
+              child: Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assests/images/P5.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0, top: 3),
+              child: Text(
+                'Gojo Saturo',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Text('and others')
+          ],
+        ),
+        SizedBox(height: 5),
         Container(
           alignment: Alignment.centerLeft,
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
